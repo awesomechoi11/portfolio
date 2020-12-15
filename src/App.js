dd9ff2e0720b8ded7cb780189883020d1e755a21
brandon <brandonchoi2016@gmail.com>
2020-12-15 01:16:42 -0800

    code splitting and loadable components

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import { IntroPage, SmallCard, AboutPage } from './components.js';
 import throttle from 'lodash.throttle';
 
 
-const OtherComponent = loadable(() => import('./OtherComponent'))
+//const OtherComponent = loadable(() => import('./OtherComponent'))
 
 import preview1 from './previews/SIGN.jpg'
 import preview2 from './previews/Seji-Preview-1.m4v'
@@ -35,25 +35,47 @@ function lerp(xi, xf, a) {
   return xi * (1 - a) + xf * a;
 }
 
+
+
+//global variables for window scrolling
+//calculated vw values
+const smallcards = 4;
+const smallcardvw = 27;
+const smallcardpadding = 260 / 19.2;//260 px to vw
+const aboutpagevw = 1500 / 19.2;
+const intropagevw = 90;
+const vwidth = (smallcardvw + smallcardpadding) * smallcards + aboutpagevw + intropagevw;
+const whole = -vwidth / 100 * window.innerWidth
+const barlength = 93.125;
+const spacing = 3.4375;
+const tablength = 10000 / vwidth
+
+window.scrollvar = {
+  smallcards: 4,//
+  smallcardvw: 27,
+  smallcardpadding: 260 / 19.2,
+  aboutpagevw: 1500 / 19.2,
+  intropagevw: 90,
+  vwidth: (smallcardvw + smallcardpadding) * smallcards + aboutpagevw + intropagevw,
+  whole: -vwidth / 100 * window.innerWidth,
+  barlength: 93.125,
+  spacing: 3.4375,
+  tablength: 10000 / vwidth
+}
+
 //handles sideways scrolling throttled
 let posthrottle = throttle(function () {
   //length of the whole thing minus repeat fillers
-  const smallcards = 4;
-  const smallcardvw = 27;
-  const smallcardpadding = 260;
-  const aboutpagevw = 1500 / 19.2;
-  const intropagevw = 90;
-  const vwidth = (smallcardvw + smallcardpadding / 19.2) * smallcards + aboutpagevw + intropagevw;
-  const whole = -vwidth / 100 * window.innerWidth
+
 
   var newpos = lerp(window.xpos, window.xdest, 0.05)
 
-  if (newpos < whole) {
-    newpos %= whole;// get new position on opposite side of window
-    window.xdest %= whole;
+  if (newpos < window.scrollvar.whole) {
+    newpos %= window.scrollvar.whole;// get new position on opposite side of window
+    window.xdest %= window.scrollvar.whole;
   } else if (newpos > 0) {
-    newpos = (newpos % whole) + whole;// get new position on opposite side of window
-    window.xdest = (window.xdest % whole) + whole;
+    newpos = (newpos % window.scrollvar.whole) + window.scrollvar.whole;// get new position on opposite side of window
+    window.xdest = (window.xdest % window.scrollvar.whole) + window.scrollvar.whole;
   }
   //smooth out position
   window.xpos = newpos
@@ -168,8 +190,36 @@ export class App extends React.Component {
 
 
 export class BottomScrollbar extends React.Component {
+
   constructor(props) {
     super(props)
+    this.mousedragevent = this.mousedragevent.bind(this)
+
+  }
+
+
+  //on drag of bottom white bar,
+  // track and change window dest
+  mousedragevent(event) {
+    //need to track difference
+    var initial = event.pageX
+    var prev = event.pageX;
+
+
+    //using let so it can be referenced to remove listener
+    let onMouseMove = function onMouseMove(event) {
+      window.xdest += ((event.pageX - prev) * 100 / 92 / window.innerWidth * window.scrollvar.whole)
+      prev = event.pageX
+    }
+
+    document.addEventListener('mousemove', onMouseMove);
+    // remove unneeded handlers
+    function removeListeners() {
+      document.removeEventListener('mousemove', onMouseMove);
+      document.getElementById('scrollbar-tab').onmouseup = null;
+    }
+    //need to bind to remove specific function from listener
+    document.onmouseup = removeListeners.bind(onMouseMove)
 
   }
 
@@ -179,65 +229,11 @@ export class BottomScrollbar extends React.Component {
       requestAnimationFrame(posthrottle)
 
       //draggable scroll start *******************
-      document.getElementById('scrollbar-tab').onmousedown = function (event) {
-        const smallcards = 4;
-        const smallcardvw = 27;
-        const smallcardpadding = 260;
-        const aboutpagevw = 1500 / 19.2;
-        const intropagevw = 90;
-        const vwidth = (smallcardvw + smallcardpadding / 19.2) * smallcards + aboutpagevw + intropagevw;
-        const whole = -vwidth / 100 * window.innerWidth
-        var initial = event.pageX
-        var prev = event.pageX;
-        function moveAt(pageX) {
-          //change window xpos
-          //console.log(initial - pageX, ' ', pageX - prev)
-          window.xdest += ((pageX - prev) * 100 / 92 / window.innerWidth * whole)
-          prev = pageX
-        }
-        function onMouseMove(event) {
-          moveAt(event.pageX);
-        }
-
-        document.addEventListener('mousemove', onMouseMove);
-        // (3) drop the ball, remove unneeded handlers
-        document.onmouseup = function () {
-          document.removeEventListener('mousemove', onMouseMove);
-          document.getElementById('scrollbar-tab').onmouseup = null;
-        };
-
-      }
+      document.getElementById('scrollbar-tab').onmousedown = this.mousedragevent
       document.getElementById('scrollbar-tab').ondragstart = function () {
         return false;
       };
-      document.getElementById('scrollbar-tab-dummy').onmousedown = function (event) {
-        const smallcards = 4;
-        const smallcardvw = 27;
-        const smallcardpadding = 260;
-        const aboutpagevw = 1500 / 19.2;
-        const intropagevw = 90;
-        const vwidth = (smallcardvw + smallcardpadding / 19.2) * smallcards + aboutpagevw + intropagevw;
-        const whole = -vwidth / 100 * window.innerWidth
-        var initial = event.pageX
-        var prev = event.pageX;
-        function moveAt(pageX) {
-          //change window xpos
-          //console.log(initial - pageX, ' ', pageX - prev)
-          window.xdest += ((pageX - prev) * 100 / 92 / window.innerWidth * whole)
-          prev = pageX
-        }
-        function onMouseMove(event) {
-          moveAt(event.pageX);
-        }
-
-        document.addEventListener('mousemove', onMouseMove);
-        // (3) drop the ball, remove unneeded handlers
-        document.onmouseup = function () {
-          document.removeEventListener('mousemove', onMouseMove);
-          document.getElementById('scrollbar-tab-dummy').onmouseup = null;
-        };
-
-      }
+      document.getElementById('scrollbar-tab-dummy').onmousedown = this.mousedragevent
       document.getElementById('scrollbar-tab-dummy').ondragstart = function () {
         return false;
       };
@@ -248,31 +244,15 @@ export class BottomScrollbar extends React.Component {
   left(index) {// calculates the left value for absolute positioning
     //relative to its index
     //console.log('index ', index)
-    const smallcards = 4;
-    const smallcardvw = 27;
-    const smallcardpadding = 260;
-    const aboutpagevw = 1500 / 19.2;
-    const intropagevw = 90;
-    const vwidth = (smallcardvw + smallcardpadding / 19.2) * smallcards + aboutpagevw + intropagevw;
-    const whole = -vwidth / 100 * window.innerWidth
-    const barlength = 93.125;
-    const spacing = 3.4375;
-    const tablength = 10000 / vwidth
-    return ((-(90 + 13.5 - 50 + ((27 + (260 / 19.2)) * index)) / 100 * window.innerWidth) / whole * barlength) + (tablength / 2)
+
+    return ((-(window.scrollvar.intropagevw + window.scrollvar.smallcardvw / 2 - 50 + ((window.scrollvar.smallcardvw + window.scrollvar.smallcardpadding) * index)) / 100 * window.innerWidth) / window.scrollvar.whole * window.scrollvar.barlength)
+      + (window.scrollvar.tablength / 2)
+
   }
   leftvw(vw) {// calculates the left value for absolute positioning
     //relative to its index
-    const smallcards = 4;
-    const smallcardvw = 27;
-    const smallcardpadding = 260;
-    const aboutpagevw = 1500 / 19.2;
-    const intropagevw = 90;
-    const vwidth = (smallcardvw + smallcardpadding / 19.2) * smallcards + aboutpagevw + intropagevw;
-    const whole = -vwidth / 100 * window.innerWidth
-    const barlength = 93.125;
-    const spacing = 3.4375;
-    const tablength = 10000 / vwidth
-    return ((-(vw) / 100 * window.innerWidth) / whole * barlength) + (tablength / 2)
+
+    return ((-(vw) / 100 * window.innerWidth) / window.scrollvar.whole * window.scrollvar.barlength) + (window.scrollvar.tablength / 2)
   }
   render() {
     const cardnames = ['seji', 'nomina', 'drop bot', 'misc'];
@@ -308,7 +288,7 @@ export class BottomScrollbar extends React.Component {
             <span key={index} left={this.left(index)} style={{ left: this.left(index) + 'vw' }}>
               <span className='title-inner'
                 onClick={() => {
-                  window.xdest = -(90 + 13.5 - 50 + ((27 + (260 / 19.2)) * index)) / 100 * window.innerWidth
+                  window.xdest = -((window.scrollvar.intropagevw + window.scrollvar.smallcardvw / 2 - 50 + ((window.scrollvar.smallcardvw + window.scrollvar.smallcardpadding) * index)) / 100 * window.innerWidth)
                 }}
               >
                 {name}
